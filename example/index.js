d9c855c17c43b44dcd914d894e1b0476fd7c2a9c
Ionică Bizău <bizauionica@gmail.com>
2016-07-08 06:35:25 +0300

    Working version

--- example/index.js
+++ example/index.js
@@ -2,4 +2,11 @@
 
 const flatColorsGroupped = require("../lib");
 
-console.log(flatColorsGroupped());
+console.log(flatColorsGroupped);
+// { blue: [ '#3498db', '#2980b9' ],
+//   teal: [ '#1abc9c', '#16a085' ],
+//   green: [ '#2ecc71', '#27ae60' ],
+//   purple: [ '#9b59b6', '#8e44ad' ],
+//   dark: [ '#34495e', '#2c3e50' ],
+//   ...
+//   gray: [ '#95a5a6', '#7f8c8d' ] }
